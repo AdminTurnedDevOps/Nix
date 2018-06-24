097729fc975b40b00f03e6cd4c51fa93b5ea24c0
AdminTurnedDevOps <mlevan1992@gmail.com>
2018-06-24 13:48:28 -0400

    First commit on short bash scripts

---
+++ Sysinfo.sh
@@ -0,0 +1,19 @@
+echo 'Current user signed in: 'whoami
+echo 'Show systemd resource'
+top | grep 'systemd' -m 1
+
+echo 'preparing host info for: ' uname -n
+echo 'Kernal: '
+uname -s
+
+echo 'Kernal Release: '
+uname -r
+
+echo 'Host Hardware: '
+uname -m
+
+echo 'Host CPU: '
+uname -p
+
+echo 'Operating System'
+uname -o
